0631829a9e123cd6fb2b40cbfd77c34290f309c9
Ant and Buffalo <antandbuffalo@gmail.com>
2017-04-26 21:40:58 +0800

    timing added

--- www/homeboard/home.board.controller.js
+++ www/homeboard/home.board.controller.js
@@ -1,9 +1,43 @@
 (function() {
-  angular.module("starter").controller("HomeBoardController", function($scope) {
+  angular.module("starter").controller("HomeBoardController", function($scope, $interval) {
 
     $scope.description = function() {
-      
+
     };
+
+    var ONE_DAY = 24 * 60 * 60 * 1000;
+    var ONE_HOUR = 60 * 60 * 1000;
+    var ONE_SEC = 60 * 1000;
+
+    function makeItTwoDigit(input) {
+      if(input < 10) {
+        return "0" + input;
+      }
+      return input;
+    };
+
+    function calculateRemaining() {
+      var today = new Date().getTime();
+      var marriageDate = new Date("2017-06-04 09:30").getTime();
+      var remaining = marriageDate - today;
+      var days = parseInt(remaining / ONE_DAY);
+
+      var daysRemainder = remaining % ONE_DAY;
+      var hours = makeItTwoDigit(parseInt(daysRemainder / ONE_HOUR));
+
+      var hoursRemaining = daysRemainder % ONE_HOUR;
+      var minutes = makeItTwoDigit(parseInt(hoursRemaining / ONE_SEC));
+
+      var minutesRemaining = hoursRemaining % ONE_SEC;
+      var seconds = makeItTwoDigit(parseInt(minutesRemaining / 1000));
+
+      $scope.remaining = days + "days " + hours +":" + minutes +":" + seconds;
+      $scope.days = days;
+      $scope.hours = hours;
+      $scope.minutes = minutes;
+      $scope.seconds = seconds;
+    };
+    $interval(calculateRemaining, 1000);
 
   });
 })();
